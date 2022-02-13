06781473f6450956010631cf929f14ae91ca5bcb
JohnVemula1998 <johnvemula7771998@gmail.com>
2022-02-13 17:30:14 +0530

    Updates

---
+++ precache-manifest.9a6ca4461d379d89c8bbb0320f99586c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54337587c954f12b4073",
+    "url": "/lataji/static/css/main.9e2f97c9.chunk.css"
+  },
+  {
+    "revision": "54337587c954f12b4073",
+    "url": "/lataji/static/js/main.52761e47.chunk.js"
+  },
+  {
+    "revision": "ebb299f65752270b62be",
+    "url": "/lataji/static/js/runtime~main.7c4b18b8.js"
+  },
+  {
+    "revision": "b7117a5d63189cdc881d",
+    "url": "/lataji/static/js/2.492b1f6f.chunk.js"
+  },
+  {
+    "revision": "1c357e22dd6f8916921c6b7812ef3fe9",
+    "url": "/lataji/index.html"
+  }
+];
